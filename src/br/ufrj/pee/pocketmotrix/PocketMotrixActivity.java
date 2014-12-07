376df5b77055c81f3a56b207903db1b8bf19769a
Limazix <blimacardoso@gmail.com>
2014-12-06 22:26:17 -0200

    Add Android Annotations 3.2

--- src/br/ufrj/pee/pocketmotrix/PocketMotrixActivity.java
+++ src/br/ufrj/pee/pocketmotrix/PocketMotrixActivity.java
@@ -1,34 +1,13 @@
 package br.ufrj.pee.pocketmotrix;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
 
+import android.support.v7.app.ActionBarActivity;
+
+@EActivity(R.layout.activity_pocket_motrix)
+@OptionsMenu(R.menu.pocket_motrix)
 public class PocketMotrixActivity extends ActionBarActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_pocket_motrix);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.pocket_motrix, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
