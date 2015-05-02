9bba53943a592d1ef45cb5fb0f65156a16359ef6
Limazix <blimacardoso@gmail.com>
2015-05-02 17:37:17 -0300

    Application for testing pocket sphinx

---
+++ pocketsphinx-android-test/src/main/java/br/ufrj/pee/pocketsphinxtest/PocketSphinxTestActivity.java
@@ -0,0 +1,15 @@
+package br.ufrj.pee.pocketsphinxtest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+@OptionsMenu(R.menu.menu_pocket_sphinx_test)
+@EActivity(R.layout.activity_pocket_sphinx_test)
+public class PocketSphinxTestActivity extends ActionBarActivity {
+
+}
